ff493cc0ee9a5df142403e521f2afb97fcd553c9
fphantam <f-phantam.outlook.com>
2022-08-31 20:23:13 +0800

    chmod hudi paylod class

--- hudi/src/main/java/org/ccf/bcdi2022/datalake_contest/HudiCustomPayload.java
+++ hudi/src/main/java/org/ccf/bcdi2022/datalake_contest/HudiCustomPayload.java
@@ -4,19 +4,25 @@ import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.generic.GenericRecordBuilder;
 import org.apache.avro.generic.IndexedRecord;
-import org.apache.hudi.common.model.OverwriteWithLatestAvroPayload;
+import org.apache.hudi.common.model.HoodieRecordPayload;
 import org.apache.hudi.common.util.Option;
 
 import java.io.IOException;
 import java.util.List;
 
-public class HudiCustomPayload extends OverwriteWithLatestAvroPayload{
+public class HudiCustomPayload implements HoodieRecordPayload<HudiCustomPayload> {
+    private GenericRecord record;
     public HudiCustomPayload(GenericRecord record, Comparable orderingVal) {
-        super(record, orderingVal);
+        this.record = record;
     }
 
-    public HudiCustomPayload(Option<GenericRecord> record) {
-        super(record); // natural order
+
+    @Override
+    public HudiCustomPayload preCombine(HudiCustomPayload oldValue) {
+        Long requests = Long.parseLong(this.record.get("requests").toString());
+        long requests1 = requests + Long.parseLong(oldValue.record.get("requests").toString());
+        this.record.put("requests", String.valueOf(requests1));
+        return this;
     }
 
     @Override
@@ -30,32 +36,31 @@ public class HudiCustomPayload extends OverwriteWithLatestAvroPayload{
         GenericRecord insertRecord = (GenericRecord) recordOption.get();
         GenericRecord currentRecord = (GenericRecord) currentValue;
 
-        if (isDeleteRecord(insertRecord)) {
-            return Option.empty();
-        } else {
-            final GenericRecordBuilder builder = new GenericRecordBuilder(schema);
-            List<Schema.Field> fields = schema.getFields();
-            fields.forEach(field -> {
-                Object value = insertRecord.get(field.name());
-                value = field.schema().getType().equals(Schema.Type.STRING) && value != null ? value.toString() : value;
-                Object defaultValue = field.defaultVal();
-                if (!overwriteField(value, defaultValue)) {
-                    if (field.name().equals("requests")) {
+        final GenericRecordBuilder builder = new GenericRecordBuilder(schema);
+        List<Schema.Field> fields = schema.getFields();
+        fields.forEach(field -> {
+            Object value = insertRecord.get(field.name());
+            value = field.schema().getType().equals(Schema.Type.STRING) && value != null ? value.toString() : value;
+            Object defaultValue = field.defaultVal();
+
+            if (field.name().equals("requests")) {
 //                        String ss = currentRecord.get(field.name()).toString();
 //                        System.out.println(ss);
-                        builder.set(field, Long.parseLong(value.toString()) + Long.parseLong(currentRecord.get(field.name()).toString()));
-                    } else if (field.name().equals("name")) {
-                        if (value.toString().equals("null")) {
-                            builder.set(field, currentRecord.get(field.name()).toString());
-                        }
-                    } else {
-                        builder.set(field, value);
-                    }
-                } else {
-                    builder.set(field, currentRecord.get(field.pos()));
+                Long aa = Long.parseLong(value.toString()) + Long.parseLong(currentRecord.get(field.name()).toString());
+                builder.set(field, aa.toString());
+            } else if (field.name().equals("name")) {
+                if (value.toString().equals("null")) {
+                    builder.set(field, currentRecord.get(field.name()).toString());
                 }
-            });
-            return Option.of(builder.build());
-        }
+            } else {
+                builder.set(field, value);
+            }
+        });
+        return Option.of(builder.build());
+    }
+
+    @Override
+    public Option<IndexedRecord> getInsertValue(Schema schema) throws IOException {
+        return Option.of(this.record);
     }
 }
